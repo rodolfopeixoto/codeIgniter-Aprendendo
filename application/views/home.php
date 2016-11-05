a0a05e9146e1ee0f3299b06d457488081c28a7a4
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-11-05 01:28:09 -0200

    First commit and Added first page home

---
+++ application/views/home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Site institucional">
+    <meta name="author" content="Rodolfo Peixoto">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"	rel="stylesheet">
+    <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css"	rel="stylesheet">
+
+<link href="<?=base_url('assets/css/home.css')?>"	rel="stylesh
+eet">
+<!--[if	lt	IE	9]><script	src="http://getbootstrap.com/assets/j
+s/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script	src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+<!--[if	lt	IE	9]>
+<script	src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script	src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+	<title>Rodolfo Peixoto - Soul Code Site Institucional</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">RODOLFO PEIXOTO</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">A Empresa</a></li>
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Trabalhe Conosco</a></li>
+        <li><a href="#">Fale Conosco</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+<div class="container">
+
+  <p>
+  	Conteúdo da página
+  </p>
+
+
+</div>
+
+
+
+	<script	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script	src="http://getbootstrap.com/assets/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
